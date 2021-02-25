b35f33e3cbf694adca421bdc3e643c4ec7043a13
harshit <harshitpandey12790@gmail.com>
2021-02-25 13:41:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+Login  = input("please enter your Login Email: ")
+password = input("please enter your password: ")
+
+if Login == "harshitpandey12790@gmail.com" and password == "2222":
+    print("Welcome Harshit!")
+else:
+    print("Wrong Password")
